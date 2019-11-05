1f2290054298cfd0e537416e3127d2c4946f9a94
unknown <marine.le.mezo@gmail.com>
2019-11-05 10:20:19 +0800

    exo3 test

---
+++ src/views/Exo3.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="exercise2">
+    EXO 3
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Exercise2',
+  }
+</script>
+
+<style scoped>
+    .if_example{
+        padding: 20px;
+    }
+</style>
